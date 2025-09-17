8eaffb371322570737fe4b0eab1c7e8a32573218
Bryson Ward <77869922+brysonbw@users.noreply.github.com>
2025-09-16 21:06:18 -0600

    Update app toaster el prop (#15)
    * Bump deps
    
    * Update pkg version
    
    * update app toaster el prop

--- src/views/home-view.ts
+++ src/views/home-view.ts
@@ -90,7 +90,7 @@ export class HomeView extends LitElement {
         newValue !== oldValue
       ) {
         if (this.appToasterElement) {
-          this.appToasterElement.queueLimit = newValue;
+          this.appToasterElement.toastsLimit = newValue;
         }
       }
     }
